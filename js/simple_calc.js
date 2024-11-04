fa8e175207d33bb3bf0b91acf7e6f7096fa94539
Luis Escarrilla <75938195+latesc@users.noreply.github.com>
2024-11-04 07:24:44 +0000

    Created assignment page for Simple Calculations

---
+++ js/simple_calc.js
@@ -0,0 +1,12 @@
+/*
+    https://daigler20.addu.edu.ph/mod/assign/view.php?id=158121
+
+    1. Fahrenheit to Celsius
+    2. Celsius to Fahrenheit
+    3. Meters to Feet
+    4. Feet to Meters
+*/
+
+function fahrenheitToCelcius(temp) {
+    
+}
